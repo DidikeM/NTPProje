e481bfc3d57ecb04e44e90b969d6cb7ccc516dc3
HIKMET ISKIFOGLU <hikmetiskifoglu01@gmail.com>
2021-12-18 02:26:42 +0200

    Naber

--- OrderAutomation/Form1.cs
+++ OrderAutomation/Form1.cs
@@ -20,7 +20,7 @@ namespace OrderAutomation
         private void Form1_Load(object sender, EventArgs e)
         {
             /* Yavru naber */
-
+            /* iidir ashko sndn */
 
 
             //Customer x = new Customer();
